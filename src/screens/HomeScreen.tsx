d6a16442b86e26e114d04ee0195a9b93b07290aa
isuyashpatel <suyashpatelmrj@gmail.com>
2023-12-29 10:47:02 +0530

    navigation to Dteails

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -47,7 +47,7 @@ const getCoffeeList = (category: string, data: any) => {
   }
 }
 
-const HomeScreen = () => {
+const HomeScreen = ({navigation}:any) => {
   const CoffeeList = useStore((state: any) => state.CoffeeList)
   const BeanList = useStore((state: any) => state.BeanList)
   const [categories, setCategories] = useState(
@@ -183,7 +183,7 @@ const HomeScreen = () => {
           keyExtractor={(item) => item.id}
           renderItem={({ item }) => {
             return (
-              <TouchableOpacity onPress={() => {}}>
+              <TouchableOpacity onPress={() => {navigation.push('Details')}}>
                 <CoffeeCard
                   id={item.id}
                   index={item.index}
