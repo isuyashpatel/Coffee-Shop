c6a9e5b26d3a0e3a35368b35c9186767d5b42a44
isuyashpatel <suyashpatelmrj@gmail.com>
2023-12-30 13:00:14 +0530

    Details Screen Image

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -1,16 +1,50 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StatusBar, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { useStore } from '../store/store'
+import { COLORS } from '../theme/theme'
+import ImageBackgroundInfo from '../components/ImageBackgroundInfo'
 
-const DetailsScreen = ({navigation,route}:any) => {
-  console.log('route=', route.params);
+const DetailsScreen = ({ navigation, route}: any) => {
   
+  const ItemOfIndex = useStore((state: any) =>
+    route.params.type == 'Coffee' ? state.CoffeeList : state.BeanList,
+  )[route.params.index]
+
+  const addToFavouriteList=useStore((state:any)=>state.addToFavouriteList);
+  const deleteFromFavouriteList=useStore((state:any)=>state.deleteFromFavouriteList);
+  
+
+  const ToggleFavourite=(favourite:boolean,type:string,id:string)=>{
+      favourite?deleteFromFavouriteList(type,id):addToFavouriteList(type,id)
+  }
+
+const BackHandler=()=>{
+  navigation.pop();
+}
+
+
+
   return (
-    <View>
-      <Text>DetailsScreen</Text>
+    <View style={styles.ScreenContainer}>
+      <StatusBar backgroundColor={COLORS.primaryBlackHex}/>
+      <ScrollView
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={styles.ScrollViewFlex}
+      >
+       <ImageBackgroundInfo EnablebackHandler={true} imagelink_portrait={ItemOfIndex.imagelink_portrait} type={ItemOfIndex.type} id={ItemOfIndex.id} favourite={ItemOfIndex.favourite} name={ItemOfIndex.name} special_ingreidient={ItemOfIndex.special_ingredient} ingredients={ItemOfIndex.ingredients} average_rating={ItemOfIndex.average_rating} ratings_count={ItemOfIndex.ratings_count} roasted={ItemOfIndex.roasted} BackHandler={BackHandler} ToggleFavourite={ToggleFavourite}/>
+      </ScrollView>
     </View>
   )
 }
 
 export default DetailsScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  ScreenContainer:{
+    flex:1,
+    backgroundColor:COLORS.primaryBlackHex,
+  },
+  ScrollViewFlex:{
+    flexGrow:1,
+  }
+})
