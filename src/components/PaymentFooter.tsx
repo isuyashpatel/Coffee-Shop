b4eb2358fcdabcf3c7df420fca7e7136dc3be6a5
isuyashpatel <suyashpatelmrj@gmail.com>
2023-12-30 14:04:49 +0530

    working on payment Button

---
+++ src/components/PaymentFooter.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+interface PriceProps{
+    price:string;
+    currency:string;
+}
+
+interface PaymentFooterProps{
+    price:PriceProps;
+    buttonPressHandler:any;
+    buttonTitle:string;
+}
+
+const PaymentFooter:React.FC<PaymentFooterProps> = ({price,buttonPressHandler,buttonTitle}) => {
+  return (
+    <View>
+      <Text>PaymentFooter</Text>
+    </View>
+  )
+}
+
+export default PaymentFooter
+
+const styles = StyleSheet.create({})
