d320fa512388db941f6438b6577940757e63b739
isuyashpatel <suyashpatelmrj@gmail.com>
2024-01-06 10:43:37 +0530

    orderHistory  init

--- src/screens/OrderHistoryScreen.tsx
+++ src/screens/OrderHistoryScreen.tsx
@@ -1,14 +1,74 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { ScrollView, StatusBar, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { useStore } from '../store/store'
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs'
+import { COLORS, SPACING } from '../theme/theme'
+import HeaderBar from '../components/HeaderBar'
+import EmptyListAnimation from '../components/EmptyListAnimation'
+import PopUpAnimation from '../components/PopUpAnimation'
 
 const OrderHistoryScreen = () => {
+  const OrderHistoryList = useStore((state: any) => state.OrderHistoryList)
+  const tabBarHeight = useBottomTabBarHeight();
+  const [showAnimation, setShowAnimation] = useState(false);
+
   return (
-    <View>
-      <Text>OrderHistoryScreen</Text>
+    <View style={styles.ScreenContainer}>
+      <StatusBar backgroundColor={COLORS.primaryBlackHex} />
+      {showAnimation ? (
+        <PopUpAnimation
+          style={styles.LottieAnimation}
+          source={require('../lottie/successful.json')}
+        />
+      ) : (
+        <></>
+      )}
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.ScrollViewFlex}
+      >
+        <View
+          style={[styles.ScrollViewInnerView, { marginBottom: tabBarHeight }]}
+        >
+          <View style={styles.ItemContainer}>
+            <HeaderBar title="Order History" />
+            {OrderHistoryList.length == 0 ? (
+              <EmptyListAnimation title={'No Order History'} />
+            ) : (
+              <View style={styles.ListItemContainer}>
+                {OrderHistoryList.map((data:any,index:any)=>(
+                  <></>
+                ))}
+                </View>)}
+          </View>
+        </View>
+      </ScrollView>
     </View>
   )
 }
 
 export default OrderHistoryScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  ScreenContainer: {
+    flex: 1,
+    backgroundColor: COLORS.primaryBlackHex,
+  },
+  LottieAnimation: {
+    height:250
+  },
+  ScrollViewFlex: {
+    flexGrow: 1,
+  },
+  ScrollViewInnerView: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  ItemContainer: {
+    flex: 1,
+  },
+  ListItemContainer:{
+    paddingHorizontal: SPACING.space_20,
+    gap: SPACING.space_20,
+  }
+})
